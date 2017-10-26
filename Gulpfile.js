0647522d880f36e1f3eab7538d5953b4fe9bc06f
Chris Buchert <chris@smartystreets.com>
2017-10-26 17:27:43 -0600

    Started migrating to Gulp for task running.

---
+++ Gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var config = {
+	scss: {
+		src: "./src/*.scss",
+		dest: "./dist/css"
+	}
+};
+
+gulp.task("dev", function () {
+	console.log("Hi world.");
+	gulp.run("transpile-scss");
+});
+
+gulp.task("transpile-scss", function () {
+	var sass = require("gulp-sass");
+	return gulp.src(config.scss.src)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest(config.scss.dest));
+});
+
+gulp.task("minify-css", function () {
+	return gulp;
+});
+
+gulp.task("minify-js", function () {
+	return gulp;
+});
+
+// TODO: Create task to upload minified CSS.
+// TODO: Create task to upload minified JS.
+// TODO: Create cleanup task.
+// TODO: Create publish task that wraps everything.
